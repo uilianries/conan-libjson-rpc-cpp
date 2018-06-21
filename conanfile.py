f35bee1c0d23793d0070e52a8fe57a6de368480a
Uilian Ries <uilian@khomp.com>
2018-06-21 16:13:40 -0300

    Add build
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- conanfile.py
+++ conanfile.py
@@ -1,85 +1,70 @@
-from conans import ConanFile
-from conans.tools import download, unzip, check_with_algorithm_sum, cpu_count
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from conans import ConanFile, tools, CMake
 import os
-import glob
 
-class JsonRPCCPPConan(ConanFile):
-    name = "jsonrpc-cpp"
-    khomp_upstream_version = "0.4"
-    khomp_package_version = "10"
-    version = "{}-{}".format(khomp_upstream_version, khomp_package_version)
-    checksums = {
-        "md5": "b01e0a9c54d5e511e33d4d5ba28bf958",
-        "sha1": "2c83097708614f08dbb13be7c0a968e5d4d6e579",
-        "sha256": "b6fd22acff6f4bcac70e2801b0f997d8a5dfcf39bb11901274e19fb8c9b1b39c"
-    }
-    khomp_src = "{}-{}.tar.bz2".format(name, khomp_upstream_version)
-    khomp_src_folder = "{}-{}".format(name, khomp_upstream_version)
-    khomp_conanfile_folder = os.path.dirname(__file__)
-    description = "JsonRpc-Cpp is an OpenSource implementation of JSON-RPC protocol in C++. JSON-RPC is a lightweight remote procedure call protocol similar to XML-RPC."
-    homepage = "https://sourceforge.net/projects/jsonrpc-cpp/"
-    url = "https://downloads.sourceforge.net/project/jsonrpc-cpp/jsonrpc-cpp/{}?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fjsonrpc-cpp%2Ffiles%2Fjsonrpc-cpp%2F{}%2Fdownload&ts=1517403885".format(khomp_src, khomp_src)
+class LibJsonRPCCPPConan(ConanFile):
+    name = "libjson-rpc-cpp"
+    version = "1.1.0"
+    description = "C++ framework for json-rpc (json remote procedure call) "
+    homepage = "https://github.com/cinemast/libjson-rpc-cpp"
+    url = "http://gitlab.khomp.corp/conan/conan-libjson-rpc-cpp"
     license = "MIT"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "txt"
+    generators = "cmake"
     options = {
-        "shared": [True, False, "Both"]
+        "shared": [True, False],
+        "fPIC": [True, False]
     }
-    default_options = "shared=True"
-    exports = "patches/*"
-    requires = "jsoncpp/[>=0.5.0-4]@khomp/stable"
+    default_options = "shared=True", "fPIC=True"
+    exports_sources = ["CMakeLists.txt", "cmake.patch"]
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+    requires = "jsoncpp/1.8.4@khomp/stable"
+    patch_once = False
 
-    def checksum(self):
-        for (algorithm, result) in self.checksums.items():
-            check_with_algorithm_sum(algorithm, self.khomp_src, result)
-
-    def patch(self):
-        khomp_patches_file = "{}/khomp-patches-applied".format(self.khomp_src_folder)
-        if not os.path.isfile(khomp_patches_file):
-            patches = glob.glob("{}/patches/*.patch".format(self.khomp_conanfile_folder))
-            patches.sort()
-            # apply patches
-            for patch_file in patches:
-                print("{}: patch source with {}".format(self.name, patch_file))
-                self.run("cd {} && patch -p0 < {}".format(self.khomp_src_folder, patch_file))
-
-            open(khomp_patches_file, "w").write("done")
+    def config_options(self):
+        if self.settings.os == 'Windows':
+            del self.options.fPIC
 
     def source(self):
-        if os.path.exists(self.khomp_src):
-            try:
-                self.checksum()
-            except:
-                os.unlink(self.khomp_src)
-                self.source()
-        else:
-            download(self.url, self.khomp_src)
-            self.checksum()
+        source_url = "https://github.com/cinemast/libjson-rpc-cpp"
+        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self.source_subfolder)
+        tools.patch(base_path=self.source_subfolder, patch_file="cmake.patch")
+        tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"), 'set(CMAKE_LIBRARY_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/lib")', "")
+        tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"), 'set(CMAKE_RUNTIME_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/bin")', "")
+        tools.replace_in_file(os.path.join(self.source_subfolder, "CMakeLists.txt"), 'set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY "${PROJECT_BINARY_DIR}/lib")', "")
 
-        if not os.path.exists(self.khomp_src_folder):
-            # NOTE: conan unzip does not support xz
-            # unzip(self.khomp_src)
-            self.run("bzcat {} | tar x".format(self.khomp_src))
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.definitions["BUILD_STATIC_LIBS"] = not self.options.shared
+        cmake.definitions["COMPILE_EXAMPLES"] = False
+        cmake.definitions["COMPILE_TESTS"] = False
+        cmake.definitions["COMPILE_STUBGEN"] = False
+        cmake.definitions["REDIS_SERVER"] = False
+        cmake.definitions["REDIS_CLIENT"] = False
+        cmake.definitions["HTTP_SERVER"] = False
+        cmake.definitions["HTTP_CLIENT"] = False
+        cmake.definitions["TCP_SOCKET_SERVER"] = True
+        cmake.definitions["WITH_COVERAGE"] = False
+        cmake.configure()
+        tools.replace_in_file(os.path.join("gen", "jsonrpccpp", "common", "jsonparser.h"), "#include <jsoncpp/json/json.h>", "#include <json/json.h>")
+        return cmake
 
     def build(self):
-        if self.settings.os == "Linux":
-            self.source()
-            self.patch()
-
-            cpppath = ""
-            libpath = ""
-
-            cpppath += ":".join(self.deps_cpp_info["jsoncpp"].include_paths)
-            libpath += ":".join(self.deps_cpp_info["jsoncpp"].lib_paths)
-
-            self.run("cd {} && scons platform=linux-gcc cpppath={} libpath={} -j{}".format(self.khomp_src_folder, cpppath, libpath, cpu_count()))
+        cmake = self.configure_cmake()
+        cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include/", src="{}/src".format(self.khomp_src_folder), keep_path=False, excludes=("jsonrpc_httpclient.h", ))
-        if self.options.shared in (True, "Both"):
-            self.copy("libjsonrpc.so", dst="lib", src=self.khomp_src_folder, keep_path=False, symlinks=False)
-        if self.options.shared in (False, "Both"):
-            self.copy("libjsonrpc.a", dst="lib", src=self.khomp_src_folder, keep_path=False, symlinks=False)
+        self.copy(pattern="LICENSE.txt", keep_path=False)
+        self.copy("*.h", src=os.path.join("source_subfolder", "src", "jsonrpccpp"), dst=os.path.join("include", "jsonrpccpp"))
+        self.copy("*.h", src=os.path.join("gen"), dst=os.path.join("include"))
+        self.copy("*.so*", dst="lib", src="lib")
+        self.copy("*.a", dst="lib", src="lib")
 
     def package_info(self):
-        self.cpp_info.libs = ["jsonrpc"]
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs.append("pthread")
